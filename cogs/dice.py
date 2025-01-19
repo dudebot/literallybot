6710325f09c5c9523d8b89b5e645092a3334542e
dudebot <2408946+dudebot@users.noreply.github.com>
2025-01-18 22:50:32 -0600

    Enhance CI configuration, add .gitignore, and improve dice and GPT commands

--- cogs/dice.py
+++ cogs/dice.py
@@ -1,27 +1,47 @@
 from discord.ext import commands
 import random
+import re
 
+
+def get_options(options):
+    if re.search(r',? ?\bor\b ?|, ?',options.lower()) is not None:
+        values = re.split(r',? ?\bor\b ?|, ?',options.lower())
+    elif ' ' in options:
+        values = options.split(' ')
+    else:
+        values = re.split(r'\W',options)
+    return [value for value in values if value != ""]
 class Dice(commands.Cog):
     """This is a cog with dice roll commands, including !random."""
     def __init__(self, bot):
         self.bot = bot
 
+
+
     @commands.command(name='random', description='Picks a random item from a space-separated list.')
     async def random(self, ctx, *, options: str):
         """Picks a random item from a space-separated list."""
-        values = options.split()
+        values = get_options(options)
         if values:
-            await ctx.send(random.choice(values))
+            if random.random()<0.05:
+                await ctx.channel.send( "All of these options are terrible. Please think about your life and try later.")
+            else:
+                await ctx.send(random.choice(values))
         else:
             await ctx.send("Please provide some options.")
 
-    @commands.command(name='diceroll', description='Rolls a dice with the specified number of sides.')
-    async def diceroll(self, ctx, sides: int):
+    @commands.command(name='diceroll', aliases=['dice','d6'], description='Rolls a dice with the specified number of sides.')
+    async def dice(self, ctx, sides: int = 6):
         """Rolls a dice with the specified number of sides."""
         if sides > 0:
             await ctx.send(random.randint(1, sides))
         else:
             await ctx.send("Please provide a valid number of sides.")
+
+    @commands.command(name='d20', description='Rolls a twenty-sided die.')
+    async def d20(self, ctx):
+        """Rolls a twenty-sided die."""
+        await ctx.send(random.randint(1, 20))
 
     @commands.command(name='multidice', description='Rolls multiple dice with the specified number of sides.')
     async def multidice(self, ctx, rolls: int, sides: int):
