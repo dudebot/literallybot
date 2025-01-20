d048912961d11fd5e030c36014094892f7596e14
dudebot <2408946+dudebot@users.noreply.github.com>
2025-01-20 04:34:15 -0600

    separate out dynamic and static cogs, add baseline memes

---
+++ cogs/dynamic/memes.py
@@ -0,0 +1,52 @@
+from discord.ext import commands
+from discord import File
+import random
+from datetime import datetime
+
+class Meme(commands.Cog):
+    """This is a cog with dice roll commands, including !random."""
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(name='squish', description='Cat command.')
+    async def should(self, ctx):
+        await ctx.send(file=File("media/squish.webm"))
+
+    @commands.command(name='ding', description='Cute command.')
+    async def ding(self, ctx):
+        await ctx.send(file=File("media/dingdingdoo.webm"))
+
+    @commands.command(name='aaa', aliases=["aaaa", "aaaaa", "aaaaaa", "aaaaaaa"], description='Hell command.')
+    async def aaa(self, ctx):
+        await ctx.send(file=File("media/aaaaaaaa_hd.mp4"))
+
+    @commands.command(name='pog', description='Pog command.')
+    async def pog(self, ctx):
+        await ctx.send(file=File("media/poggers.mp4"))
+
+    @commands.command(name='hi', aliases=["yahallo", "hello"], description='hi :3 command.')
+    async def yahallo(self, ctx):
+        await ctx.send(file=File("media/yahallo.mp4"))
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if (message.guild 
+            and (message.guild.id == 191762537438511104 or message.guild.id == 1046929409455693905) # todo use configuration
+            and message.content.startswith('"') 
+            and message.content.endswith('"')):
+
+            toggled = []
+            for i, c in enumerate(message.content[1:-1].replace(' ', '  ')):
+                toggled.append(c.upper() if i % 2 else c.lower())
+
+            display_text = "".join(toggled).replace('  ', ' ')
+            nickname = message.author.nick or message.author.name
+            title = random.choice(["Someone Important","Chief Furry","Alcoholic",
+                                   "Literally Who","Basically Hitler","Mayor of Foofgens",
+                                   "Peta","Some random weeb"])
+            await message.channel.send(f"> {display_text}\n{nickname}\n{title}, {datetime.today().year}")
+
+async def setup(bot):
+    """Every cog needs a setup function like this."""
+    await bot.add_cog(Meme(bot))
